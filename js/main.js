0a3bcb0a2410fadcaa179604cd3da15eaa59be29
Tech-A11 <Tech-A11@Tech-A11s-MacBook-Air.local>
2017-11-06 11:13:06 +0000

    initial commit + basic layout of page

---
+++ js/main.js
@@ -0,0 +1,36 @@
+function Connect4() {
+
+
+
+
+
+
+//calling the bringGame function
+bringGame();
+
+
+//Main screen button which hides the button and will bring up the game
+function bringGame(){
+  $("button").click(function(){
+    console.log("hey, I have been clicked");
+  })
+}
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// hey there
+
+Connect4();
