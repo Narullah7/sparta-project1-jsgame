65c6b969c11eacd97ac6f82d28eda1fd164dbd7d
Tech-A11 <Tech-A11@Tech-A11s-MacBook-Air.local>
2017-11-07 05:26:06 +0000

    made improvements, code is working

--- js/main.js
+++ js/main.js
@@ -17,35 +17,35 @@
   // }
   // });
 
+  x = true;
+  y = false;
+
+
+  firstColumn();
+function firstColumn(){
   $('.click').on('click', function(){
-     console.log('working');
-     var input = $(".1").eq();
-     for (input = 0; input<4 ; input++){
-     if (input % 2 === 0) {
-       $(".1").addClass("yellowCircle");
-       console.log("This is red");
-     } else if (input % 2 != 0) {
-       $(".1").addClass("redCircle");
-       console.log("this is yellow");
-     } else {
-       console.log("Error");
-     }
-   }
-   });
+    $(".1").each(function(index){
 
+    if ((index % 2 === 0)  ){
+      changeTurn();
+      $(this).html("").attr("class","redCircle");
+    } else if (index % 2 != 0) {
+      changeTurn();
+      $(this).html("").attr("class","yellowCircle");
+    } else {
+      console.log("Error");
+    }
+    })
+  })
+}
 
+//changes the turn of the players
+function changeTurn() {
+    x = !x;
+    y = !y;
+}
 
-
-
-
-
-
-
-//
-// function changeTurn() {
-//    player1 = false;
-//    player2 = true;
-// }
+changeTurn();
 
 
 
