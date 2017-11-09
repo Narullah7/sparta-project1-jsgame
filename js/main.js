a3d29c64e9298d9340fc04b90510d32f67d86e8d
Tech-A11 <Tech-A11@Tech-A11s-MacBook-Air.local>
2017-11-09 10:39:39 +0000

    improved indentation

--- js/main.js
+++ js/main.js
@@ -1,157 +1,77 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
-//   $(".click").on("click",function(){
-//     console.log("Cliked");
-//     var column = $(".column1");
-//     var box1 = $(column[3]);
-//     var box2 = $(column[2]);
-//     var box3 = $(column[1]);
-//     var box4 = $(column[0]);
-//
-//     counter++;
-//     if (!box1.hasClass("redCircle") && !box1.hasClass("yellowCircle")){
-//       box1.addClass("redCircle");
-//     } else if (!box2.hasClass("redCircle") && // //         //    box2.hasClass("yellowCircle")) {
-//       box2.addClass("yellowCircle");
-//     } else if (!box3.hasClass("redCircle") && !box3.hasClass("yellowCircle")){
-//       box3.addClass("redCircle");
-//     } else if (!box4.hasClass("redCircle") && !box4.hasClass("yellowCircle")) {
-//       box4.addClass("yellowCircle");
-//     }
-//   });
-//
-// function gameStart(){
-//   YellowOrRedListener();
-//
-// }
-//
-//
-// function YellowOrRedListener(){
-//   $(".click").click(YellowOrRed);
-// }
-//
-// function YellowOrRed(){
-//
-//   var column = $(".column1");
-//   var box1 = $(column[3]);
-//   var box2 = $(column[2]);
-//   var box3 = $(column[1]);
-//   var box4 = $(column[0]);
-//
-//
-//   if (colum.html().length === 0){
-//     if (counter % 2 ===0){
-//
-//     }
-//   }
-// }
-//
+  // global variables
+  var player1 = true;
+  var player1color = "rgb(86, 151, 255)";
 
-// global variables
-var player1 = true;
-var player1color = "rgb(86, 151, 255)";
+  var player2 = false;
+  var player2color = "rgb(255, 255, 0)";
 
-var player2 = false;
-var player2color = "rgb(255, 255, 0)";
+  //game is running
+  var gameRunning = true;
+  var table = $("table tr");
 
+  function RedOrYellow(row,col,color){
+    // gets the individual table cell by index in a table
+    return table.eq(row).find("td").eq(col).find("button").css("background-color",color);
+  }
 
-//game is running
-var gameRunning = true;
-var table = $("table tr");
+  function returnColor(row,col){
+    return table.eq(row).find("td").eq(col).find("button").css("background-color");
+  }
 
-function RedOrYellow(row,col,color){
-  // gets the individual table cell by index in a table
-  return table.eq(row).find("td").eq(col).find("button").css("background-color",color);
-}
-
-function returnColor(row,col){
-  return table.eq(row).find("td").eq(col).find("button").css("background-color");
-}
-
-//checks a column and returns the one that is empty or background color = gray
-function checkColumn(col){
-  var colorChecker = returnColor(4,col);
-  //starts checking from the bottom of the column first then checked the ones above it
-  for (var i = 4; i > -1 ; i--) {
-    colorChecker = returnColor(i,col);
-    if (colorChecker === "rgb(128, 128, 128)"){
-      return i
+  //checks a column and returns the one that is empty or background color = gray
+  function checkColumn(col){
+    var colorChecker = returnColor(4,col);
+    //starts checking from the bottom of the column first then checked the ones above it
+    for (var i = 4; i > -1 ; i--) {
+      colorChecker = returnColor(i,col);
+      if (colorChecker === "rgb(128, 128, 128)"){
+        return i
+      }
     }
   }
-}
-// start with player 1
-var currentPlayer = 1;
-var currentColor = player1color;
+  // start with player 1
+  var currentPlayer = 1;
+  var currentColor = player1color;
 
-$(".board button").on("click",function(){
-  var Chosen = $(this).closest("td").index();
-  var bottomColumn = checkColumn(Chosen);
+  $(".board button").on("click",function(){
+    var Chosen = $(this).closest("td").index();
+    var bottomColumn = checkColumn(Chosen);
 
-  RedOrYellow(bottomColumn,Chosen,currentColor);
+    RedOrYellow(bottomColumn,Chosen,currentColor);
 
-  currentPlayer = currentPlayer * -1
+    currentPlayer = currentPlayer * -1
 
-  if (currentPlayer === 1){
-    currentColor = player1color;
-  } else {
-    currentColor = player2color;
+    if (currentPlayer === 1){
+      currentColor = player1color;
+    } else {
+      currentColor = player2color;
+    }
+  })
+
+  // check the winner if all columns are vertically, horizontally and diagonally the same.
+  // except when the background-color = gray and it goes out of the page.
+  function Checkwinner(a,b,c,d) {
+    return ((a === b) && (a === c) && (a === d) && (a !== "rgb(128, 128, 128)"))
   }
-})
 
+  // console log the winner
+  function winner(rowInd,colInd){
+  console.log("winner");
+  }
+  //checking vertically.
+  function horizontalCheck(){
+    //checking the rows from index 0 to 4
+    for (var i = 0; i< 4; i++){
+      //checking the columns from index 0 to 4
+      for (var j = 0; i < 4; j++) {
+        if (Checkwinner(returnColor(i,j), returnColor(i,j+1), returnColor(i,j+2), returnColor(i,j+3))){
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        }
+      }
+    }
+  }
 
 });
