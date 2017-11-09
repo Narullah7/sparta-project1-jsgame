cebc24a087e6ee6c8bda261123d99610b577d8ef
Tech-A11 <Tech-A11@Tech-A11s-MacBook-Air.local>
2017-11-09 09:41:47 +0000

    main part of the game works

--- js/main.js
+++ js/main.js
@@ -48,20 +48,56 @@ $( document ).ready(function() {
 // }
 //
 
+// global variables
+var player1 = true;
+var player1color = "rgb(86, 151, 255)";
+
+var player2 = false;
+var player2color = "rgb(255, 255, 0)";
+
+
 //game is running
 var gameRunning = true;
 var table = $("table tr");
 
 function RedOrYellow(row,col,color){
   // gets the individual table cell by index in a table
-  return table.eq(row).find("td").eq(col).find("button").attr("class",color);
+  return table.eq(row).find("td").eq(col).find("button").css("background-color",color);
 }
 
-function currentColor(row,col){
+function returnColor(row,col){
   return table.eq(row).find("td").eq(col).find("button").css("background-color");
 }
 
-//take a column and returns 
+//checks a column and returns the one that is empty or background color = gray
+function checkColumn(col){
+  var colorChecker = returnColor(4,col);
+  //starts checking from the bottom of the column first then checked the ones above it
+  for (var i = 4; i > -1 ; i--) {
+    colorChecker = returnColor(i,col);
+    if (colorChecker === "rgb(128, 128, 128)"){
+      return i
+    }
+  }
+}
+// start with player 1
+var currentPlayer = 1;
+var currentColor = player1color;
+
+$(".board button").on("click",function(){
+  var Chosen = $(this).closest("td").index();
+  var bottomColumn = checkColumn(Chosen);
+
+  RedOrYellow(bottomColumn,Chosen,currentColor);
+
+  currentPlayer = currentPlayer * -1
+
+  if (currentPlayer === 1){
+    currentColor = player1color;
+  } else {
+    currentColor = player2color;
+  }
+})
 
 
 
